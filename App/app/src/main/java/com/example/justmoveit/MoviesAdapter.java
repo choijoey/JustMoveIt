5ba1e025c1d9f800f5725ccdd79ac38feb3e07bd
SSAFYCYJ <dakadern@gmail.com>
2022-07-27 22:09:25 +0900

    FEAT:ToolBar 구현
    Former-commit-id: e8226d28b81238f3769e3c426fd6b3a9b04bd80a [formerly e38b38eef23289f9af7b59c311cd0e6a0a35fcd1]
    Former-commit-id: 9c61a9727c0915ae23bba9f32aba9a8118ec0bbb

--- App/app/src/main/java/com/example/justmoveit/MoviesAdapter.java
+++ App/app/src/main/java/com/example/justmoveit/MoviesAdapter.java
@@ -11,7 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-import com.bumptech.glide.Glide;
+
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.squareup.picasso.Picasso;
 
