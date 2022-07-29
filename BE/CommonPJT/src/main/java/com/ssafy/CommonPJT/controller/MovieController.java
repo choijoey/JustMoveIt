f2e90a481dd74d46c3d735c74cb92d03953d6a86
jongil512 <jongil519@gmail.com>
2022-07-29 11:28:13 +0900

    connect swagger
    Former-commit-id: 5e682d091bfc1ab2954b842b7a79cfb94e3a5969

--- BE/CommonPJT/src/main/java/com/ssafy/CommonPJT/controller/MovieController.java
+++ BE/CommonPJT/src/main/java/com/ssafy/CommonPJT/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.ssafy.CommonPJT.controller;
 
 import com.ssafy.CommonPJT.domain.Movie;
 import com.ssafy.CommonPJT.service.MovieService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +19,13 @@ public class MovieController {
 
     private final MovieService movieService;
 
+    @ApiOperation(value = "영화 목록", notes = "영화 목록을 출력한다.", response = List.class)
     @GetMapping("/")
     public List<Movie> getMovieList() {
         return movieService.findMovies();
     }
 
+    @ApiOperation(value = "영화", notes = "영화 정보를 출력한다.", response = Movie.class)
     @GetMapping("/{id}")
     public Movie getMovieById(@PathVariable Long id) {
         return movieService.findOne(id);
