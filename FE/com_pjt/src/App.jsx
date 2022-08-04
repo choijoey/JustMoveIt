94fb8b5e824d33969c8e4d291ce0efbc16e6e9ca
DESKTOP-KVCQHCD\multicampus <sw133v@naver.com>
2022-08-04 11:50:36 +0900

    develop-FE first commit
    Former-commit-id: 7692861424441370349b4e233101911e07a26ea2

--- FE/com_pjt/src/App.jsx
+++ FE/com_pjt/src/App.jsx
@@ -6,6 +6,8 @@ import Grid from '@mui/material/Grid';
 import { MovieDetail } from './route/MovieDetail'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { Container } from '@material-ui/core';
+import styles from "./App.css";
+
 
 // function Field() {
   
@@ -51,6 +53,7 @@ axios.get('http://localhost/justmoveit/movies/')
   // }
 
   let movie
+  let times = []
   let movie_time = []
   let movie_time_tickets = []
   let seats_info = []
@@ -72,20 +75,27 @@ axios.get('http://localhost/justmoveit/movies/')
     for (const iterator of movie_time_tickets) {
       seats_info.push( iterator.seat )
     }
-    console.log(seats_info)
     seats_info.sort()
     console.log(seats_info)
     console.log(seats_info[0][0])
 
+
     for (const iterator of temp_data) {
+      // movie_time.push(
+      //   <Button>{iterator.startTime}</Button>
+      // )
+      times.push(iterator.startTime)
+    }
+    times.sort()
+
+    for (const time of times) {
       movie_time.push(
-        <button>{iterator.startTime}</button>
+        <Button>{ time }</Button>
       )
     }
-    //이거 동기식으로 콜백요청을 해야 할것으로 보임.
-
+    // movie_time.sort()
+  
     // for (const iterator of info_data) {
-
     //   slides.push(<img src={ iterator['img'] }></img>)
     //   text_data.push(<h1>{iterator['title']}</h1>)
     // }
@@ -135,78 +145,195 @@ axios.get('http://localhost/justmoveit/movies/')
     seats.push(<br></br>)
   }
 
+  const sr = ["A","B","C","D",]
+  const sc = [1,2,3,4,5]
+  let sr_info = []
+  let sc_info = []
+  for (const iterator of sr) {
+    sr_info.push(<span>{iterator}</span>)
+  }
+  for (const iterator of sc) {
+    sr_info.push(<span>{iterator}</span>)
+  }
+
+
+
 
 const App = () => {
   const movieTitle = useRef(null)
+  
   
   const callback = function(index){
     movieTitle.current.innerText = info_data[index]['title'] 
   }
   
-  return (
-    // <div className='App'>
-    //   <br />
-    //   <br />
-    //   <br />
-    //   <br />
-
-    //   <Carousel slides={slides} arrows={false} autoplay={false} interval={1000} onSlideChange={callback}/>
-
-    //   <Grid
-    //     container
-    //     direction="column"
-    //     justifyContent="center"
-    //     alignItems="center">
-    //     <br></br>
-    //     <br></br>
-    //     <br></br>
-
-    //     <h1 ref={ movieTitle }></h1>'
-
-    //       <button>영화 선택</button>
-
-    //     <br></br>
-    //     <br></br>
-    //     <br></br>
-    //     <br></br>
-
-    //     <br></br>
-    //     <br></br>
-    //     <br></br>
-    //   </Grid>
-    // </div>
+  const face = function(){
+    const spawn = require('child_process').spawn;
+    const result = spawn('python', ['gad.py']);
     
-    // <div className='App'>
-    //    <Grid
-    //      container
-    //      direction="column"
-    //      justifyContent="center"
-    //      alignItems="center">
-
-    //     <h1>{ movie.title }</h1>
-    //     <img src={ movie.img }></img>
-    //     <h3>{ movie.summary }</h3>
-
-    //     { time }
-    //     <div>
-          
-    //     </div>
-        
-    //     </Grid>
-    // </div>
-
+    result.stdout.on('data', function(data) {
+      console.log(data.toString());
+    });
+  
+    result.stderr.on('data', function(data) {
+      console.log(data.toString());
+    });
+  }
+  
+  return (
     <div className='App'>
-      <h1> 좌석 선택 </h1>
+      {/* <br />
+      <br />
+      <br />
+      <br />
+
+      <Carousel slides={slides} arrows={false} autoplay={false} interval={1000} onSlideChange={callback}/>
+
+      <div>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <h1 ref={ movieTitle }></h1>'
+
+          <button>영화 선택</button>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <br></br>
+        <br></br>
+        <br></br>
+      </div> */}
+
+      {/* <div>
+        <img src={ movie.img }></img>
+        <h1>{ movie.title }</h1>
+        <h3>{ movie.summary }</h3>
+
+        { movie_time }
+        <br></br>
+        <button>좌석 선택</button>
+        <div>
+          
+        </div>
+      </div> */}
+        
+
       {/* 대충 스크린 모양
-          비어있는 좌석 모양, 선택된 좌석 모양, 예약된 좌석 모양
+      비어있는 좌석 모양, 선택된 좌석 모양, 예약된 좌석 모양
               1 2 3 4 5
-            A
+            A 
             B
             C
             D
-             */}
-      { seats }
+          */}
+
+      {/* <div className='container'>
+      <div className='row'>
+      <div className='column row-2'></div>
+      <div className='column row-10'></div>
+      <div className='column row2'></div>
+      <div className='column row10'></div>
+      </div>
+    </div> */}
+
+      <h1> 좌석 선택 </h1>
+      {/* {sr_info}
+      {sc_info} */}
+      {/* { seats } */}
+      <div className={ styles.screen }></div>
+      
+      {/* <ul className={styles.showcase}>
+      <li>
+      <div className={ styles.seat }></div>
+      <small>N/A</small>
+      </li>
+      <li>
+      <div className={styles.seat.selected}></div>
+      <small>Selected</small>
+      </li>
+      <li>
+      <div className={styles.seat.occupied}></div>
+      <small>Occupied</small>
+      </li>
+      </ul>
+      
+      <div className={styles.container}>
+      <div className={styles.screen}></div>
+      
+      <div className={ styles.row }>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      </div>
+      <div className={ styles.row }>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat.occupied}></div>
+      <div className={ styles.seat.occupied}></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      </div>
+      <div className={ styles.row }>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat.occupied}></div>
+      <div className={ styles.seat.occupied}></div>
+      </div>
+      <div className={ styles.row }>
+      <div className={ styles.seat }></div>
+      <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          </div>
+          <div className={ styles.row }>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat.occupied}></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat.occupied}></div>
+          <div className={ styles.seat }></div>
+          </div>
+          <div className={ styles.row }>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat }></div>
+          <div className={ styles.seat.occupied}></div>
+          <div className={ styles.seat.occupied}></div>
+          </div>
+        </div> */}
+
+      {/* <Button onClick={ face }>하이하이</Button> */}
+
+
+
     </div>
   )
 }
 export default App
+
+
+//  정보 받아오는것은 콜백 요청으로 동기화 된상태에서 렌더링 하게끔 해줘야 하는거같음.
