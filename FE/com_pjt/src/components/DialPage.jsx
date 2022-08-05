efa2764c2bae34e22ae9166a624efead8ce83af7
DESKTOP-KVCQHCD\multicampus <sw133v@naver.com>
2022-08-05 17:16:31 +0900

    FEAT:seats_picker
    Former-commit-id: acee11bfd71635fea145470f21c5cd2a82020d8e [formerly a4234c05cb5a989c5d02185b5004ead7e657f389]
    Former-commit-id: 1be841b84c87e2b4694bbcc14054c4aad3397071

---
+++ FE/com_pjt/src/components/DialPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function DialPage() {
+
+    return(
+        <div className="DialPage">
+            <h1>일반</h1>
+            
+            <h1>청소년</h1>
+            
+            <h1>경로</h1>
+
+            <h1>우대</h1>
+            
+        </div>
+    )
+}
+export default DialPage
