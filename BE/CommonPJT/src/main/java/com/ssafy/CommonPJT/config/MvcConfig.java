8188322d79aca82ccdba95ef31fca9b1a2578b84
SSAFYCYJ <dakadern@gmail.com>
2022-08-10 17:42:39 +0900

    FEAT: hidden method 설정 추가
    Former-commit-id: 4a280bb2aa6e898dabb4d253ef5b0118c70a4de5 [formerly 76d92c9f27f3ae6955fce688be8a7ba5f467ad95]
    Former-commit-id: e0d21dd8cfb573783acf3d5b4cad453f658032d4

--- BE/CommonPJT/src/main/java/com/ssafy/CommonPJT/config/MvcConfig.java
+++ BE/CommonPJT/src/main/java/com/ssafy/CommonPJT/config/MvcConfig.java
@@ -2,7 +2,7 @@ package com.ssafy.CommonPJT.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
