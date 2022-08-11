ae36c2f784a7b1c9e11deac6fea18ce255683166
Hyoseon Kim <apso123@naver.com>
2022-08-11 16:13:29 +0900

    임시
    Former-commit-id: af84024cdf5baf5483827ee5b2f083cc041f1939 [formerly 3e37d9b3fb2b995b222b50de39fd1819aa72ec8f]
    Former-commit-id: cf68ead6ea6c0297658acb61ddb50552d19016ba

--- App/app/src/main/java/com/example/justmoveit/activity/LoginActivity.java
+++ App/app/src/main/java/com/example/justmoveit/activity/LoginActivity.java
@@ -2,14 +2,21 @@ package com.example.justmoveit.activity;
 
 import static com.example.justmoveit.activity.LoadingActivity.userSP;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.justmoveit.R;
 import com.example.justmoveit.model.User;
@@ -29,6 +36,10 @@ import com.kakao.util.exception.KakaoException;
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+    private SharedPreferences.Editor editor;
+    private Context context;
+    private static final int PERMISSIONS_REQUEST_CODE = 1;
+
     private final ISessionCallback mSessionCallback = new ISessionCallback() {
         @Override
         public void onSessionOpened() {
@@ -55,8 +66,6 @@ public class LoginActivity extends AppCompatActivity {
                     Intent intent = new Intent(LoginActivity.this, MyTicketListActivity.class);
                     UserAccount account = result.getKakaoAccount();
 
-                    SharedPreferences.Editor editor = userSP.edit();
-
                     User user = new User(account.getProfile().getProfileImageUrl(),
                             account.getProfile().getNickname(), account.getEmail(),
                             Objects.requireNonNull(account.getGender()).getValue(),
@@ -64,8 +73,6 @@ public class LoginActivity extends AppCompatActivity {
 
                     Gson gson = new Gson();
                     editor.putString("user_info", gson.toJson(user));
-                    // Todo: 전화번호 얻기
-                    editor.putString("phone_number", "01052584112");
                     editor.apply();
 
                     startActivity(intent);
@@ -85,8 +92,86 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        context = this;
+
+        editor = userSP.edit();
         Session.getCurrentSession().addCallback(mSessionCallback);
         Session.getCurrentSession().checkAndImplicitOpen();// 자동 로그인
+
+        if (!chkPermission()) {
+            // 휴대폰 정보는 TelephonyManager 를 이용
+            TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+            // READ_PHONE_NUMBERS 또는 READ_PHONE_STATE 권한을 허가 받았는지 확인
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(context, tm.getLine1Number() + "위험 권한 승인함 ", Toast.LENGTH_SHORT).show();
+                editor.putString("phone_number", tm.getLine1Number());
+                editor.apply();
+                Log.d(String.valueOf(LoginActivity.this), "전화번호 : [ getLine1Number ] >>> " + tm.getLine1Number());
+            }
+        }
+
+    }
+
+    public boolean chkPermission() {
+        // 위험 권한을 모두 승인했는지 여부
+        boolean mPermissionsGranted = false;
+        String[] mRequiredPermissions = new String[1];
+        // 사용자의 안드로이드 버전에 따라 권한을 다르게 요청합니다
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // 11 이상인 경우
+            mRequiredPermissions[0] = Manifest.permission.READ_PHONE_NUMBERS;
+        } else {
+            // 10 이하인 경우
+            mRequiredPermissions[0] = Manifest.permission.READ_PRECISE_PHONE_STATE;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 필수 권한을 가지고 있는지 확인한다.
+            mPermissionsGranted = hasPermissions(mRequiredPermissions);
+
+            // 필수 권한 중에 한 개라도 없는 경우
+            if (!mPermissionsGranted) {
+                // 권한을 요청한다.
+                ActivityCompat.requestPermissions(LoginActivity.this, mRequiredPermissions, PERMISSIONS_REQUEST_CODE);
+            }
+        } else {
+            mPermissionsGranted = true;
+        }
+
+        return mPermissionsGranted;
+    }
+
+    public boolean hasPermissions(String[] permissions) {
+        // 필수 권한을 가지고 있는지 확인한다.
+        for (String permission : permissions) {
+            if (checkCallingOrSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            // 권한을 모두 승인했는지 여부
+            boolean chkFlag = false;
+            // 승인한 권한은 0 값, 승인 안한 권한은 -1을 값으로 가진다.
+            for (int g : grantResults) {
+                if (g == -1) {
+                    chkFlag = true;
+                    break;
+                }
+            }
+
+            // 권한 중 한 개라도 승인 안 한 경우
+            if (chkFlag) {
+                chkPermission();
+            }
+        }
     }
 
     @Override
@@ -107,22 +192,22 @@ public class LoginActivity extends AppCompatActivity {
     // 토큰 관리
     protected void requestAccessToken() {
         AuthService.getInstance()
-            .requestAccessTokenInfo(new ApiResponseCallback<AccessTokenInfoResponse>() {
-                @Override
-                public void onSessionClosed(ErrorResult errorResult) {
-                    Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
-                }
+                .requestAccessTokenInfo(new ApiResponseCallback<AccessTokenInfoResponse>() {
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                    }
 
-                @Override
-                public void onFailure(ErrorResult errorResult) {
-                    Log.e("KAKAO_API", "토큰 정보 요청 실패: " + errorResult);
-                }
+                    @Override
+                    public void onFailure(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "토큰 정보 요청 실패: " + errorResult);
+                    }
 
-                @Override
-                public void onSuccess(AccessTokenInfoResponse result) {
-                    Log.i("KAKAO_API", "사용자 아이디: " + result.getUserId());
-                    Log.i("KAKAO_API", "남은 시간(s): " + result.getExpiresInMillis());
-                }
-            });
+                    @Override
+                    public void onSuccess(AccessTokenInfoResponse result) {
+                        Log.i("KAKAO_API", "사용자 아이디: " + result.getUserId());
+                        Log.i("KAKAO_API", "남은 시간(s): " + result.getExpiresInMillis());
+                    }
+                });
     }
 }
