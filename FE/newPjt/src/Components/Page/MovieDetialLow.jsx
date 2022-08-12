c4d3087118e7acac02dc530b2a0011a09f608074
DESKTOP-KVCQHCD\multicampus <sw133v@naver.com>
2022-08-12 13:48:38 +0900

    new_pjt
    Former-commit-id: bd029234e9f49f06b2421e004b91fee456f075a6 [formerly 0901e53b20ba9712b831240e862d8295612f5817]
    Former-commit-id: 4f0910c6905063e913b12753ca0c091e9d622de4

---
+++ FE/newPjt/src/Components/Page/MovieDetialLow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MovieDetailLow() {
+  return (
+    <div className="MovieDetailLow">
+        <h1>여기는 디테일 로우</h1>
+    </div>
+  );
+}
+
+export default MovieDetailLow;
