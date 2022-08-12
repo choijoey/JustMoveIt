1011a7ffb28bd7050d62538d83e4c32574e3ec00
강동관 <dpfwpf2999@naver.com>
2022-08-12 13:14:17 +0900

    FEAT: websocket test with python-spring
    Former-commit-id: d659ecbc46c0957fda7dee5f84ec020b01b81e76 [formerly c8cd60e9ec783c018c14033995ff6f45935128c2]
    Former-commit-id: df148fdf149b6ceb3770a017ce89e2878bca6871

---
+++ IoT/Websocket.test/test.py
@@ -0,0 +1,19 @@
+from socket import *
+from select import select
+import sys
+import time
+from websocket import create_connection
+
+
+def sendmessage(msg):
+
+    try:
+        ws = create_connection("ws://localhost:8081/api/socket")
+        print("Sending")
+        ws.send(msg)
+        ws.close()
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    sendmessage('Hello')
