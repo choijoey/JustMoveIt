1e4c0645c7edd56b284d29a12f7c8ff609b0c685
DESKTOP-KVCQHCD\multicampus <sw133v@naver.com>
2022-08-18 01:16:10 +0900

    0818merge
    Former-commit-id: 8204e867fa2ab8f28f627f1d1937709c7436980a [formerly 05c9f016819a81a19a7481c112631503882171b7]
    Former-commit-id: 3b6923e1e6620c9fe4459e012f578e72859224a4

--- FE/newPjt/src/App.jsx
+++ FE/newPjt/src/App.jsx
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 
 import "./App.css";
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Router from "./Router";
 import Face from "./Components/AI/Face";
 import Pose from "./Components/AI/Pose";
+// import $ from "jquery";
 import { CompressOutlined } from "@mui/icons-material";
 
 function App() {
@@ -14,6 +15,11 @@ function App() {
   const [AppGender, setGender] = useState("1");
   const [dirction, setDirction] = useState("1");
   const axios = require("axios").default;
+  // const navigate = useNavigate();
+  // const { navigation } = this.props;
+
+  // const dh = 190;
+
   // console.log("https://i7d207.p.ssafy.io/api/movies/" + movie_code[0]);
   // axios
   //   .get("https://i7d207.p.ssafy.io/api/movies/" + movie_code[0])
@@ -35,7 +41,17 @@ function App() {
   //       console.log(err, "센서값 못받아옴");
   //     })
   //     .then(function (response) {
-  //       console.log(response.data);
+  //       // console.log(response.data.lengthValue);
+  //       if (window.location.pathname == "/") {
+  //         // console.log("아 응애");
+  //         // navigation.navigate("default");
+  //         // navigate("/default");
+  //         if (response.data.lengthValue >= 150) {
+  //           window.location.assign(window.location.href + "default");
+  //         } else if (response.data.lengthValue < 150 and 100 < response.data.lengthValue) {
+  //           window.location.assign(window.location.href + "low");
+  //         }
+  //       }
   //     });
   // }, 1000);
 
@@ -67,6 +83,7 @@ function App() {
 
   function onMessage(msg) {
     if (!isOpen(websocket)) return;
+
     var data = msg.data;
     var message = null;
 
@@ -77,6 +94,7 @@ function App() {
   function isOpen(ws) {
     return ws.readyState === ws.OPEN;
   }
+
   /////////////////////////////////////////////////////////////
 
   return (
@@ -87,8 +105,9 @@ function App() {
       </h3>
       <h3>방향은 현재 {dirction} 을 가리키고 있다!</h3> */}
 
-      <Face setAge={setAge} setGender={setGender}></Face>
-      <Pose setDirction={setDirction}></Pose>
+      {/* <Face setAge={setAge} setGender={setGender}></Face>
+      <Pose setDirction={setDirction}></Pose> */}
+
       <div class="container">
         <div>
           <div id="msgArea" class="col"></div>
