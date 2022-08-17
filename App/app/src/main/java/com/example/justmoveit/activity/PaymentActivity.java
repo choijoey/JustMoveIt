576743639adea5574c7ab2ffda3055f1d65800ba
김효선 <apso123@naver.com>
2022-08-17 21:45:13 +0900

    FIX: paymentActivity url 수정
    Former-commit-id: af630b79611dd65d285d60ed3b93d44bb245f2fa [formerly 1ebdd6e0369105ac25b8e0b95c638507a9f8621f]
    Former-commit-id: 0cb2ee5ed21f4acea68c0c33b9c67909d0199864

--- App/app/src/main/java/com/example/justmoveit/activity/PaymentActivity.java
+++ App/app/src/main/java/com/example/justmoveit/activity/PaymentActivity.java
@@ -79,9 +79,9 @@ public class PaymentActivity extends AppCompatActivity {
             Integer quantity = 1;
             Integer total_amount = PRODUCT_PRICE;
             Integer tax_free_amount = 0;
-            String approval_url = "https://naver.com";
+            String approval_url = "https://developers.kakao.com";
             String cancel_url = "https://naver.com";
-            String fail_url = "https://naver.com";
+            String fail_url = "https://google.com";
 
             service.paymentReady(cid, partner_order_id, partner_user_id, item_name, quantity,
                     total_amount, tax_free_amount, approval_url, cancel_url, fail_url).enqueue(new Callback<PayReady>() {
@@ -163,6 +163,7 @@ public class PaymentActivity extends AppCompatActivity {
             view.loadUrl(url);
             return false;
         }
+
     }
 
     private void postTicketToServer(Ticket ticket){
